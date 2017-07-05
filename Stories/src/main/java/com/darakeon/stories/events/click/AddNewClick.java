4d66debfe14469b6c15809f8c0cc134bb51c2ff6
Lucas Duarte <lucas.duarte@inspira.com.br>
2017-07-05 18:50:48 -0300

    Forcing refresh of list when add sibling
    by [Dara Keon (Isis Keon)] in [2016-03-05 18:20:47 (-03:00)]

--- Stories/src/main/java/com/darakeon/stories/events/click/AddNewClick.java
+++ Stories/src/main/java/com/darakeon/stories/events/click/AddNewClick.java
@@ -1,6 +1,9 @@
 package com.darakeon.stories.events.click;
 
 import android.view.View;
+import android.view.ViewParent;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class AddNewClick implements View.OnClickListener
 {
@@ -17,6 +20,29 @@ public class AddNewClick implements View.OnClickListener
     public void onClick(View v)
     {
         item.AddSibling(arg);
+        refresh(v);
+    }
+
+    private void refresh(View view)
+    {
+        if (view != null)
+        {
+            if (view instanceof ListView)
+            {
+                ListView listView = (ListView) view;
+                ListAdapter adapter = listView.getAdapter();
+                listView.setAdapter(adapter);
+                listView.requestLayout();
+                return;
+            }
+
+            ViewParent parent = view.getParent();
+
+            if (parent instanceof View)
+            {
+                refresh((View) parent);
+            }
+        }
     }
 
     public interface IChildWithSibs
